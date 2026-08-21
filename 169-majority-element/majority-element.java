336ea2edd4074ddaf60308218dacff083460cb93
Preetham V K <preethamvk0108@gmail.com>
2026-08-21 10:41:32 +0530

    Time: 12 ms (37.46%) | Memory: 52.8 MB (73.37%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate=0;
-        int count=0;
-        for(int num:nums)
+        // //Boyer Moore Algorithm 
+        // int candidate=0;
+        // int count=0;
+        // for(int x:nums)
+        // {
+        //     if(count==0)
+        //         candidate=x;
+        //     if(x==candidate)
+        //     count++;
+        //     else 
+        //     count--;
+        // }
+        // return candidate;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        int ans=0;
+        for(int x:nums)
         {
-            if(count==0)
-            candidate=num;
-            if(num==candidate)
-            count++;
-            else 
-            count--;
+            map.put(x,map.getOrDefault(x,0)+1);
         }
-        return candidate;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+        {
+            if(entry.getValue()>n/2)
+            ans=entry.getKey();
+        }
+        return ans;
     }
 }
